02ea439fda5ee7f818b4c5930e5c3d8498e0472d
ferriones <ferriones@gmail.com>
2020-06-02 03:16:22 +0000

    se creo el documento app.js y la clase miembro

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// javascript aqui
+class Miembro {
+
+    constructor(nombre, edad, genero, profesion){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+        this.profesion = profesion;
+    
+    }
+    getEdad() {
+        return `Hola soy ${this.nombre} mi edad es ${this.edad}` ;
+
+
+    }
+    
+}
+const Magdaleno = new Miembro ("Magdaleno", 26, "Masculino" , "programador");
+console.log(Magdaleno.getEdad());
+
